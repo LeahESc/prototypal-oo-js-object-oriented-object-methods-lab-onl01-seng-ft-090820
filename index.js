8194cd38bc35d2797157d8eb631fef67a0140a73
LeahESc <leahemily1@gmail.com>
2020-12-28 09:25:52 -0800

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-class BoardMember{
-    constructor(name, homeState, training){
-        this.name = name
-        this.homeState = homeState
-        this.training = training
-    }
+
+function BoardMember(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
 }
+
 
 BoardMember.prototype.veto = function() {
     return "No, I must disagree"
